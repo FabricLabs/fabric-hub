9f771fced3570a70d045ac393f6968e7c7d821dc
Eric Martindale <eric@ericmartindale.com>
2025-01-25 01:19:37 -0800

    Extend test timeout to 10s

--- tests/hub.integration.js
+++ tests/hub.integration.js
@@ -5,6 +5,8 @@ const Hub = require('../services/hub');
 
 describe('hub.fabric.pub', function () {
   describe('Hub', function () {
+    this.timeout(10000);
+
     it('can smoothly create an instance of the Hub service', function () {
       const hub = new Hub();
       assert.ok(hub);
